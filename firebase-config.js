1d8985357be27d6c91fdbdbd6eaf8c0db6ac5f7f
ace696134 <142068983+ace696134@users.noreply.github.com>
2025-04-11 17:10:38 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,21 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
-import { initializeAppCheck, ReCaptchaV3Provider } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app-check.js";
+// Use the modular App Check SDK here:
+import { initializeAppCheck, ReCaptchaEnterpriseProvider } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app-check.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCMHbxQszAY5DykXY-mPxr1jENu_sWp1NE",
-  authDomain: "backend-e5b61.firebaseapp.com",
-  projectId: "backend-e5b61",
-  storageBucket: "backend-e5b61.appspot.com",
-  messagingSenderId: "1081485617349",
-  appId: "1:1081485617349:web:073f122bcb6501701068ac",
-  measurementId: "G-XFKGCDJ51G"
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  // etc.
 };
 
 const app = initializeApp(firebaseConfig);
 
+// Initialize App Check using modular SDK
 initializeAppCheck(app, {
-  provider: new ReCaptchaV3Provider('6LewEBUrAAAAAILpiN3OfFxxK7CbO6VHht2gJgHC'),
+  provider: new ReCaptchaEnterpriseProvider('6LewEBUrAAAAAILpiN3OfFxxK7CbO6VHht2gJgHC'),
   isTokenAutoRefreshEnabled: true,
 });
 
