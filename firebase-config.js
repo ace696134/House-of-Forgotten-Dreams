987a5cbd68a0c7bfb2383f34b80035c42f1cc94d
ace696134 <142068983+ace696134@users.noreply.github.com>
2025-04-12 21:42:53 +0000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,12 @@
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCMHbxQszAY5DykXY-mPxr1jENu_sWp1NE",
+  authDomain: "backend-e5b61.firebaseapp.com",
+  databaseURL: "https://backend-e5b61-default-rtdb.firebaseio.com",
+  projectId: "backend-e5b61",
+  storageBucket: "backend-e5b61.appspot.com",
+  messagingSenderId: "1081485617349",
+  appId: "1:1081485617349:web:073f122bcb6501701068ac",
+  measurementId: "G-XFKGCDJ51G"
 };
 
 firebase.initializeApp(firebaseConfig);
