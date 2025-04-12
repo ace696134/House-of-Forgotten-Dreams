dda5c8c36d085113e33c92ca2b53bc7347002dbb
ace696134 <142068983+ace696134@users.noreply.github.com>
2025-04-12 02:00:02 -0400

    Add files via upload

--- auctions.js
+++ auctions.js
@@ -1,14 +1,18 @@
 
-import { auth, db } from './firebase-config.js';
-
 function fetchAuctions() {
   db.collection('auctions').orderBy('endsAt', 'asc').get()
     .then(snap => {
       const auctions = snap.docs.map(d => ({ id: d.id, ...d.data() }));
-      console.log('Auctions:', auctions);
-      // applyFilters(); // Uncomment if this function is defined elsewhere
+      console.log(auctions); // You could render auctions here
     })
     .catch(err => console.error('Fetch auctions error', err));
 }
 
-fetchAuctions();
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+    console.log("User is logged in:", user.email);
+    fetchAuctions();
+  } else {
+    console.log("No user logged in");
+  }
+});
