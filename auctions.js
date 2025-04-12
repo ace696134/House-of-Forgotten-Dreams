a1f88a4700c281799cea588d4e7effc48523599f
ace696134 <142068983+ace696134@users.noreply.github.com>
2025-04-12 05:38:23 +0000

    Update auctions.js

--- auctions.js
+++ auctions.js
@@ -1,13 +1,14 @@
+// auctions.js
+import { auth, db } from './firebase-config.js';
 
 let isAdmin = false;
+let auctions = [];
 
-// Wait for Firebase Auth to be ready before fetching auctions
 auth.onAuthStateChanged(user => {
   isAdmin = user?.email === 'houseofforgottendreams@yahoo.com';
   fetchAuctions();
 });
 
-// Original fetchAuctions function, now runs after auth state is ready
 function fetchAuctions() {
   db.collection('auctions').orderBy('endsAt', 'asc').get()
     .then(snap => {
@@ -16,3 +17,4 @@ function fetchAuctions() {
     })
     .catch(err => console.error('Fetch auctions error', err));
 }
+
